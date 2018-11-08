6c9ec1c208e61045322e097a1c53c95369dae153
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-08 11:58:20 +0100

    Fetch user rides from backend when signed in

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -1,10 +1,28 @@
 <template>
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
-    <p>You should only get here if you're authenticated!</p>
+    <p v-if="notSignedIn" class="alert alert-warning">You should only get here if you're authenticated!</p>
   </div>
 </template>
+<script>
+  import axios from 'axios';
 
+  export default {
+    data() {
+      return {
+        rides: [],
+      };
+    },
+    created() {
+      this.$store.dispatch('fetchUserRides');
+    },
+    computed: {
+      notSignedIn(){
+        return this.$store.getters.signInError;
+      }
+    }
+  }
+</script>
 <style scoped>
   h1, p {
     text-align: center;
