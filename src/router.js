cdfb582b678b6a185fca8561968b65a11e540fe1
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-08 11:21:41 +0100

    Setup routes guard to prevent unauthorized access
    - guard '/dashboard' for unauthorized access
    - redirect to '/' from '/signin', '/signup' if user already exists and is
    signed in

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+
 import WelcomePage from './components/welcome/welcome.vue'
 import DashboardPage from './components/dashboard/dashboard.vue'
 import SignupPage from './components/auth/signup.vue'
@@ -10,9 +11,30 @@ Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: WelcomePage },
-  { path: '/signup', component: SignupPage },
-  { path: '/signin', component: SigninPage },
-  { path: '/dashboard', component: DashboardPage }
+  { path: '/signup', component: SignupPage , beforeEnter(to, from, next){
+      if (localStorage.token){
+        next('/')
+      } else {
+        next()
+      }
+    }
+  },
+  { path: '/signin', component: SigninPage, beforeEnter(to, from, next){
+      if (localStorage.token){
+        next('/')
+      } else {
+        next()
+      }
+    }
+  },
+  { path: '/dashboard', component: DashboardPage, beforeEnter(to, from, next) {
+      if (localStorage.token){
+        next()
+      } else {
+        next('/signin')
+      }
+    }
+  }
 ]
 
 export default new VueRouter({mode: 'history', routes})
