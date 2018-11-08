4e1bbf812aa10778a8226e9841a82e662aabcbdd
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-08 11:27:55 +0100

    Set Authorization header token in Axios global config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 import router from './router'
 import store from './store'
+
+axios.defaults.baseURL = 'http://54.38.36.242'
+axios.interceptors.request.use(config => {
+  config.headers.common['Authorization'] = localStorage.getItem('token')
+  return config;
+})
 
 new Vue({
   el: '#app',
