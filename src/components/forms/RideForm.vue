98b6dc2911a0093a335d587e57aacde085c4810a
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-09 12:01:31 +0100

    New Ride component
    - new component that creates a new ride

--- src/components/forms/RideForm.vue
+++ src/components/forms/RideForm.vue
@@ -1,45 +1,43 @@
 <template>
-  <form @submit.prevent>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col"></th>
-          <th scope="col">Start address</th>
-          <th scope="col">Destination address</th>
-          <th scope="col">Price</th>
-          <th scope="col">Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row"></th>
-          <th scope="row"></th>
-          <td><input
-                    type="text"
-                    id="start_address"
-                    v-model="start_address">
-          </td>
-          <td><input
-                    type="text"
-                    id="destination_address"
-                    v-model="destination_address"></td>
-          <td><input
-                    type="text"
-                    id="price"
-                    v-model="price">
-          </td>
-          <td><input
-                    type="text"
-                    id="date"
-                    v-model="date">
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </form>
+  <div class="container">
+    <p v-show="showErrorAlert" class="alert alert-danger">Invalid input data. Please try again.</p>
+    <form @submit.prevent="onSubmit" class="form-inline">
+    <label class="sr-only" for="inlineFormInputName2">Start Address</label>
+    <input 
+      type="text"
+      class="form-control mb-5 mr-sm-5"
+      id="inlineFormInputName2"
+      placeholder="Start Address"
+      v-model="start_address">
+
+    <label class="sr-only" for="inlineFormInputName2">Destination Address</label>
+    <input 
+      type="text" 
+      class="form-control mb-5 mr-sm-5" 
+      id="inlineFormInputName2" 
+      placeholder="Destination Address"
+      v-model="destination_address">
+    <label class="sr-only" for="inlineFormInputName2">Price</label>
+    <input 
+      type="text" 
+      class="form-control mb-5 mr-sm-5" 
+      id="inlineFormInputName2" 
+      placeholder="Price"
+      v-model="price">
+    <label class="sr-only" for="inlineFormInputName2">Date</label>
+    <input 
+      type="text" 
+      class="form-control mb-5 mr-sm-5" 
+      id="inlineFormInputName2" 
+      placeholder="Date"
+      v-model="date">
+  <button type="submit" class="btn btn-primary mb-5">Submit</button>
+</form>
+  </div>
 </template>
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
     data(){
       return {
@@ -48,6 +46,22 @@
         price: '',
         date: ''
       }
+    },
+    methods: {
+      onSubmit () {
+        const formData = {
+          start_address: this.start_address,
+          destination_address: this.destination_address,
+          price: this.price,
+          date: this.date
+        }
+        this.$store.dispatch('createRide', formData);
+      }
+    },
+    computed: {
+      ...mapGetters([
+          'showErrorAlert'
+      ])
     }
   }
 </script>
