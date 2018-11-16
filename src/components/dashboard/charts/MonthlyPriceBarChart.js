820ce9a8d2c4fa9863cfb0338df6316e11f9defd
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-16 09:37:14 +0100

    Add gradient to the chart bars

--- src/components/dashboard/charts/MonthlyPriceBarChart.js
+++ src/components/dashboard/charts/MonthlyPriceBarChart.js
@@ -3,12 +3,24 @@ import { Bar } from 'vue-chartjs'
 export default {
   extends: Bar,
   mounted () {
+    this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+    this.gradient2 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+
+    this.gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)')
+    this.gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)');
+    this.gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
+    
+    this.gradient2.addColorStop(0, 'rgba(0, 231, 255, 0.9)')
+    this.gradient2.addColorStop(0.5, 'rgba(0, 231, 255, 0.25)');
+    this.gradient2.addColorStop(1, 'rgba(0, 231, 255, 0)');
+    
     this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
         {
           label: 'Total price',
-          backgroundColor: '#fa923f',
+          backgroundColor: this.gradient,
           data: [40, 20, 12, 39, 20, 40, 39, 80, 40, 20, 12, 30]
         }
       ]
