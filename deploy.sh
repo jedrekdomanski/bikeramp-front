ef19fa1b3b12dc611d937f5bea972e3c0d903ec3
JounQin <admin@1stg.me>
2018-11-28 23:19:41 +0800

    feat: changes demo for GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ set -e
 # build
 npm run build
 
+# target build.js is in different path on development and production of GitHub Pages
+# A better way should be using [html-webpack-plugin](https://github.com/jantimon/html-webpack-plugin)
+cp index.pages.html dist/index.html
+
 # navigate into the build output directory
 cd dist
 
@@ -20,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:jedrekdomanski/bikeramp-front.git master:gh-pages
 
 cd -
