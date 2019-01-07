6cd545d3d8e1fcad5f754ab6efc72aa2e28b4aa1
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-07 15:37:48 +0100

    Add /profile and /profile/edit routes

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
-
 import WelcomePage from './components/welcome/welcome.vue'
 import DashboardPage from './components/dashboard/dashboard.vue'
 import SignupPage from './components/auth/signup.vue'
 import SigninPage from './components/auth/signin.vue'
+import User from './components/userData/userProfile.vue'
+import UserForm from './components/forms/userForm.vue'
 
 Vue.use(VueRouter)
 
@@ -28,6 +28,22 @@ const routes = [
     }
   },
   { path: '/dashboard', component: DashboardPage, beforeEnter(to, from, next) {
+      if (localStorage.token){
+        next()
+      } else {
+        next('/signin')
+      }
+    }
+  },
+  { path: '/profile', component: User, beforeEnter(to, from, next){
+      if (localStorage.token){
+        next()
+      } else {
+        next('/signin')
+      }
+    }
+  },
+  { path: '/profile/edit', component: UserForm, beforeEnter(to, from, next){
       if (localStorage.token){
         next()
       } else {
