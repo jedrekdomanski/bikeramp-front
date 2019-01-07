11447f68b2aecd5c0f2e85648db8e61c57468e80
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-07 15:39:28 +0100

    Remove comments from charts component

--- src/components/dashboard/charts/MonthlyPriceChart.js
+++ src/components/dashboard/charts/MonthlyPriceChart.js
@@ -11,18 +11,7 @@ export default {
       
     }
   },
-  mounted () {
-    // this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
-    // this.gradient2 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
-
-    // this.gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)')
-    // this.gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)');
-    // this.gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
-    
-    // this.gradient2.addColorStop(0, 'rgba(0, 231, 255, 0.9)')
-    // this.gradient2.addColorStop(0.5, 'rgba(0, 231, 255, 0.25)');
-    // this.gradient2.addColorStop(1, 'rgba(0, 231, 255, 0)');
-    
+  mounted () {    
     this.renderChart(this.chartdata, this.options)
   }
 }
