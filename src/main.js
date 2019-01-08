5ce3940339381b81086205e6a75192c2216add96
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-08 16:14:55 +0100

    Update BaseURL

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import router from './router'
 import store from './store/store'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'http://54.38.36.242'
 axios.interceptors.request.use(config => {
   config.headers.common['Authorization'] = localStorage.getItem('token')
   return config;
