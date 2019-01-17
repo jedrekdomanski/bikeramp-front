ebc980407fa93803e4649645e859af56ac0d648c
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-17 09:12:53 +0100

    Refactor Signin - move HTML to a template

--- src/components/auth/signin.vue
+++ src/components/auth/signin.vue
@@ -1,30 +1,4 @@
-<template>
-  <div id="signin">
-    <p v-if="showErrorAlert" class="alert alert-danger">Invalid email or password. Please try again.</p>
-    <div class="signin-form">
-      <form @submit.prevent="onSubmit">
-        <div class="input">
-          <label for="email">Email</label>
-          <input
-                  type="email"
-                  id="email"
-                  v-model="email">
-        </div>
-        <div class="input">
-          <label for="password">Password</label>
-          <input
-                  type="password"
-                  id="password"
-                  v-model="password">
-        </div>
-        <div class="submit">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</template>
-
+<template src="./signin.html"></template>
 <script>
   export default {
     data () {
