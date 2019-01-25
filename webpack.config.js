e9f4132d5c06b8624bd6bb1769151cadbd68c380
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-25 21:32:37 +0100

    Add vue-style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.css/,
+        use: ['vue-style-loader', 'css-loader'] // BOTH are needed!
       }
     ]
   },
